69ab758274c3222c3635f592aa58e36b7bf5d03f
Kamron Hays <kamron.hays@live.com>
2018-11-17 08:05:01 -0500

    Add post model, controller, and view

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 64 }
   validates :body, presence: true, length: { maximum: 1024 }
   belongs_to :user
+  # Retrieve the posts from the database in descending order of creation time.
+  default_scope -> { order(created_at: :desc) }
 end
